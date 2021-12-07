03c62687981e5b869ce902d42b0fe72a01095845
James <jmes.spncer@gmail.com>
2021-12-07 19:25:06 +0000

    🎵: the look - metronomy (#1)

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)

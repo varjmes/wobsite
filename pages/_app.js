3a79feb3ed21611ef55614f259d651a083e95023
James Spencer <jmes.spncer@gmail.com>
2022-01-01 21:45:11 +0000

    feat: render markdown posts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,49 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import Image from 'next/image'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import styles from '../styles/page.module.css'
+
+function App({ Component, pageProps }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>james spencer</title>
+        <meta name="description" content="james spencer's personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Component {...pageProps} />
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://twitter.com/varjmes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>
+        |
+        <a
+          href="https://github.com/varjmes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+        |
+        <a
+          href="https://instagram.com/varjmes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          instagram
+        </a>
+      </footer>
+    </div>
+  )
 }
 
-export default MyApp
+export default App

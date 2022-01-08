b81e14e01a51d573942b1d075270e7707d5a072f
James Spencer <jmes.spncer@gmail.com>
2022-01-08 08:51:24 +0000

    👂: no such thing as a fish #15

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,65 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 
 import '../styles/globals.css'
 import styles from '../styles/page.module.css'
 
-function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>james spencer</title>
-        <meta name="description" content="james spencer's personal website" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>About</li>
+        </ul>
+      </nav>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Component {...pageProps} />
+        </main>
 
-      <main className={styles.main}>
-        <Component {...pageProps} />
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://twitter.com/varjmes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          twitter
-        </a>
-        |
-        <a
-          href="https://github.com/varjmes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github
-        </a>
-        |
-        <a
-          href="https://instagram.com/varjmes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          instagram
-        </a>
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <a
+            href="https://twitter.com/varjmes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            twitter
+          </a>
+          |
+          <a
+            href="https://github.com/varjmes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github
+          </a>
+          |
+          <a
+            href="https://instagram.com/varjmes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            instagram
+          </a>
+          |
+          <a
+            href="mailto:jmes.spncer@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            email
+          </a>
+        </footer>
+      </div>
+    </>
   )
 }
 

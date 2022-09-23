7e624aec9aacc577935ed2034ac31b1c62398d0d
James <jmes.spncer@gmail.com>
2022-09-23 16:49:26 +0100

    feat: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import { pageview } from '../lib/gtm'
 
 import '../styles/globals.css'
 import styles from '../styles/page.module.css'
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+     pageview(url)
+    }
+    
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GTM_ID } from '../lib/gtm'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -12,6 +13,22 @@ class MyDocument extends Document {
         <Head>
           <meta name="description" content="james spencer's personal website" />
           <link rel="icon" href="/favicon.ico" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
